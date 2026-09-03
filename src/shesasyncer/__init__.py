1e633f0514d83c850281f94868ad2a25dd3175f6
Tereroa <kuresa_afamasaga@yahoo.com>
2026-09-03 18:38:30 +1000

    Add public package API

---
+++ src/shesasyncer/__init__.py
@@ -0,0 +1,4 @@
+from .core.pipeline import AlignmentPipeline
+from .core.models import AlignmentResult, LyricLine, Timing, AlignmentEvidence
+
+__all__ = ["AlignmentPipeline", "AlignmentResult", "LyricLine", "Timing", "AlignmentEvidence"]
