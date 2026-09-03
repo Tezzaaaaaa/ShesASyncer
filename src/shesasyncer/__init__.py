e10dba5ef976915ae0662b478c339e028ab00223
Tereroa <kuresa_afamasaga@yahoo.com>
2026-09-03 18:43:14 +1000

    Expose adaptive aligner and engine interfaces

--- src/shesasyncer/__init__.py
+++ src/shesasyncer/__init__.py
@@ -1,4 +1,17 @@
+from .core.adaptive import AdaptiveAligner
 from .core.pipeline import AlignmentPipeline
 from .core.models import AlignmentResult, LyricLine, Timing, AlignmentEvidence
+from .engines.adapters import TimedSegment, TimingEngine
+from .engines.whisperx import WhisperXEngine
 
-__all__ = ["AlignmentPipeline", "AlignmentResult", "LyricLine", "Timing", "AlignmentEvidence"]
+__all__ = [
+    "AdaptiveAligner",
+    "AlignmentPipeline",
+    "AlignmentResult",
+    "LyricLine",
+    "Timing",
+    "AlignmentEvidence",
+    "TimedSegment",
+    "TimingEngine",
+    "WhisperXEngine",
+]
